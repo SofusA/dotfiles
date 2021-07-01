456cd325c1d25e0eb0ab485027f24c4f8b5a4012
Sofus Addington <sofus@addington.dk>
2021-07-01 12:31:32 +0200

    update

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,3 +1,6 @@
 set -gx EDITOR kak
 alias python python3
 alias upgrade 'sudo dnf upgrade -y; flatpak upgrade -y'
+alias upgradesb 'rpm-ostree upgrade; flatpak upgrade -y'
+alias vpn 'sudo openconnect --protocol=gp gpgatewayhbk.bksv.com -u saddington'
+
