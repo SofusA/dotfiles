6a871498283949c881211607cb6f1656d1c4927e
Sofus Addington <soad@simcorp.com>
2023-07-13 16:32:16 +0200

    update_repo

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
-set -gx EDITOR hx
-set -U fish_greeting
-
 alias upgrade 'sudo dnf upgrade -y; flatpak upgrade -y; distrobox upgrade -a'
 alias ls 'exa -1 --color=always --group-directories-first'
+
+set -gx EDITOR hx
+set -U fish_greeting
