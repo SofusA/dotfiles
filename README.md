13600a6b8e332d5388fbc4cdf8d88dfc697b7ef2
Sofus Addington <sofus@addington.dk>
2023-07-15 20:18:50 +0200

    update

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
 # My configuration files
 Configurations for tools that i use
 
-Dependencies:
-- `helix`
-- `exa`
-- `distrobox`
-- `ripgrep`
-- `rip`
-- `bat`
-- `vscode-langservers-extracted`
-
-# Todo
-- Make a list of all cargo tools and an update script
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,8 @@
-alias upgrade 'flatpak upgrade -y; home-manager -f ~/dotfiles/home.nix switch && ~/dotfiles/replace_config'
+alias upgrade 'home-manager -f ~/dotfiles/home.nix switch && ~/dotfiles/replace_config'
 alias ls 'exa -1 --color=always --group-directories-first'
+alias hxg 'nix run github:helix-editor/helix'
 
 set -gx EDITOR hx
 set -U fish_greeting
+
+set PATH /var/home/sofusa/.npm-packages/bin $PATH
