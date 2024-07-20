a847368930784bb99dcbe5c4fa4e6908d72606d9
Sofus Addington <sofus@addington.dk>
2024-07-20 16:13:59 +0200

    use flake utils for ccase

--- nix/tools/ccase/flake.nix
+++ nix/tools/ccase/flake.nix
@@ -1,13 +1,17 @@
 {
-  description = "A simple Nix flake to install ccase";
+  description = "ccase";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.ccase= with nixpkgs.legacyPackages.x86_64-linux; 
-      rustPlatform.buildRustPackage {
-        pname = "ccase";
-        version = "0.4.1";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.ccase = pkgs.rustPlatform.buildRustPackage {
+          pname = "ccase";
+          version = "0.4.1";
 
           src = pkgs.fetchFromGitHub {
             owner = "rutrum";
@@ -16,7 +20,8 @@
             hash = "sha256-TQJkvANms/5Mzh1J4qsEYOrlML17dVv7MYEoN4Z/gm0=";
           };
 
-        cargoHash = "sha256-TAjKBUzyJUtSwj3EkD0+/owEWrmxws58axS2YMlG0MY=";
-      };
-  };
+          cargoHash = "sha256-TAjKBUzyJUtSwj3EkD0+/owEWrmxws58axS2YMlG0MY=";
+        };
+      }
+    );
 }
