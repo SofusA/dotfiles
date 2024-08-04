85e6917b6ddde6cf51107e16f13506c0769dadb0
Sofus Addington <sofus@addington.dk>
2024-08-04 21:12:47 +0200

    Use unstable again

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "My development flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   inputs.flatpak-xdg-utils.url = "path:./tools/flatpak-xdg-utils";
