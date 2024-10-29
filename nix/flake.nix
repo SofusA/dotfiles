8b5181ddb9db40f2c21f16f1e775d87ffcb11d8a
Sofus Addington <sad@cvation.com>
2024-10-29 09:38:17 +0100

    Remove zellij again

--- nix/flake.nix
+++ nix/flake.nix
@@ -11,7 +11,6 @@
   inputs.roslyn-language-server.url = "github:sofusa/roslyn-language-server/main";
   inputs.helix.url = "github:sofusa/helix-pull-diagnostics/pull-diagnostics";
   inputs.jujutsu.url = "github:martinvonz/jj";
-  inputs.zellij.url = "github:a-kenji/zellij-nix";
   inputs.bicep.url = "github:sofusa/bicep-language-server-nix";
   inputs.azure-pipelines.url = "github:sofusa/azure-pipelines-language-server-nix";
 
@@ -26,7 +25,6 @@
     roslyn-language-server,
     helix,
     jujutsu,
-    zellij,
     bicep,
     azure-pipelines
   }:
@@ -79,7 +77,6 @@
             pkgs.sd
             pkgs.ripgrep
             pkgs.yaml-language-server
-            zellij.packages.${system}.zellij
 
             # git
             pkgs.gitui
