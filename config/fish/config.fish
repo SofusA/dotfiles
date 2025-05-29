daa0c1fc855e3e28c7cd51f7ce008f522e2d0ac5
Sofus Addington <sofus@addington.dk>
2025-05-29 10:54:30 +0200

    fish prompt

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,9 +14,20 @@ alias np "jj git push"
 alias npc "jj git push -c @"
 alias nf "jj git fetch"
 alias nn "jj new"
+alias nbm "nb move --from @- --to @"
 alias n jj
 
 alias ls "eza -1 --icons --group-directories-first"
+
+function fish_prompt
+    set -l last_status $status
+    set -l stat
+    if test $last_status -ne 0
+        set stat (set_color red)"[$last_status]"(set_color normal)
+    end
+
+    string join '' -- (set_color blue) (prompt_pwd) (set_color normal) $stat '> '
+end
 
 # Todo: parameterize this
 function cms
